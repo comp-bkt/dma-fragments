449912def19a4bc04a5da148eda1d5657f143453
pat <p@p>
2023-01-30 12:42:04 +0000

    added a savedInstance to record the current colour

--- app/src/main/java/com/leedsbeckett/basicfragment/MainActivity.kt
+++ app/src/main/java/com/leedsbeckett/basicfragment/MainActivity.kt
@@ -3,13 +3,14 @@ package com.leedsbeckett.basicfragment
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.view.Menu
 
 class MainActivity : AppCompatActivity(), OnColourChoiceHandler {
 
-    
+    private var currentColour:Int? = null
+
     companion object {
         const val TAG = "Lifecycles"
+        const val ARG_COLOUR = "current_colour"
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -44,6 +45,8 @@ class MainActivity : AppCompatActivity(), OnColourChoiceHandler {
         if(fragment is ColourDisplayFragment) {
             fragment.updateUI(choice)
         }
+
+        this.currentColour=choice
     }
 
     override fun switchToColourPicker() {
@@ -84,6 +87,20 @@ class MainActivity : AppCompatActivity(), OnColourChoiceHandler {
     override fun onDestroy() {
         super.onDestroy()
         Log.v(TAG, "Activity - onDestroy")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt(ARG_COLOUR, currentColour!!)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        val fragment = supportFragmentManager.findFragmentByTag("fragment_colour_display")
+        if(fragment is ColourDisplayFragment) {
+            this.currentColour = savedInstanceState.getInt(ARG_COLOUR)
+            fragment.updateUI(this.currentColour!!)
+        }
     }
 
 }
